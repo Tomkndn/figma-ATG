902d235d5e6c7fb0388e9774229b947b5a0562e7
Tomkndn <ntamsoy66@gmail.com>
2023-12-08 03:42:23 +0530

    created all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
 import HeroSection from "./Components/HeroSection";
-import Article from "./Components/Article";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App(){
     return(
         <>
             <Navbar/>
             <HeroSection/>
-            <Article/>
+            <article className="container">
+                <Header/>
+                <Main/>
+            </article>
         </>
     )
 }
